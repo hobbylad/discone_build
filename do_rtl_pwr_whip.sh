c1c2ca7f92caf469a404523493e2513382dd0645
Marcos <marcos@example.com>
2017-11-11 13:16:37 +0000

    Discone/whip cripts for measuring of rx power at different RTL-SDR gains.

---
+++ do_rtl_pwr_whip.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rtl_power -f 25M:1700M:1M -i 1m -g  0 -1 wip_25_1700_1M_g00.csv  > cap_whip.txt
+rtl_power -f 25M:1700M:1M -i 1m -g 10 -1 wip_25_1700_1M_g10.csv >> cap_whip.txt
+rtl_power -f 25M:1700M:1M -i 1m -g 20 -1 wip_25_1700_1M_g20.csv >> cap_whip.txt
+rtl_power -f 25M:1700M:1M -i 1m -g 30 -1 wip_25_1700_1M_g30.csv >> cap_whip.txt
+rtl_power -f 25M:1700M:1M -i 1m -g 40 -1 wip_25_1700_1M_g40.csv >> cap_whip.txt
+rtl_power -f 25M:1700M:1M -i 1m -g 50 -1 wip_25_1700_1M_g50.csv >> cap_whip.txt
+pi@raspberrypi:~ $
